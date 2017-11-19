e3bbb81bd2e077b356e22d0c6c1695a3074aa09b
Arto Nieminen <arto.nieminen@mediamaisteri.com>
2017-11-19 18:10:09 +0200

    Add upgrade script for old settings (< Moodle 3.3)

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-$plugin->version = 2017011901;
-$plugin->requires = 2012062500;
-$plugin->release = 'auth/saml version of 19-01-2017';
+$plugin->version = 2017111300;
+$plugin->requires = 2017050500;
+$plugin->release = 'auth/saml version of 13-11-2017';
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'auth_saml';
